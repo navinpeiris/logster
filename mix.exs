8e52bb7d85435640e46fe47d1797610583cd790f
Navin Peiris <navinpeiris@gmail.com>
2016-03-31 17:31:41 +0530

    Prepare for relase

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Logster.Mixfile do
   def project do
     [app: :logster,
      version: "0.1.0",
+     description: "Easily parsable log output for Plug and Phoenix applications",
+     package: package,
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -20,5 +22,12 @@ defmodule Logster.Mixfile do
 
      {:mix_test_watch, "~> 0.2", only: :dev},
      {:ex_unit_notifier, "~> 0.1", only: :test}]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Navin Peiris"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/navinpeiris/logster"}]
   end
 end
