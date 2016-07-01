41c8dca000e86a42e7bac948a9744ba51556d4cc
Navin Peiris <navinpeiris@gmail.com>
2016-07-01 10:43:56 +0530

    Updating version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Logster.Mixfile do
 
   def project do
     [app: :logster,
-     version: "0.2.0",
+     version: "0.3.0",
      name: "Logster",
      description: "Easily parsable log output for Plug and Phoenix applications",
      package: package,
