f8893212f543bba1c97ffe55d53e5ac1a040ef38
Navin Peiris <navinpeiris@gmail.com>
2020-05-03 10:24:34 +0530

    Update deps

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Logster.Mixfile do
       {:jason, "~> 1.1"},
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev},
-      {:mix_test_watch, "~> 0.2", only: :dev},
+      {:mix_test_watch, "~> 1.0", only: :dev},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_unit_notifier, "~> 0.1", only: :test}
     ]
